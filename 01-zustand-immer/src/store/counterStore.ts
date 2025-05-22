aa97b72e0bedd46c0ecacf1ae3c4bac861b2fe66
Kevin Chaparro <kevinchaparro@ElKevin2.local>
2025-05-22 16:54:15 -0700

    Fix:Uso de immer

--- 01-zustand-immer/src/store/counterStore.ts
+++ 01-zustand-immer/src/store/counterStore.ts
@@ -1,35 +1,34 @@
 import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer';
 
-
-interface Post{
-    id:number;
-    title:string;
-    body:string;
+interface Post {
+    id: number;
+    title: string;
+    body: string;
 }
 interface counterState {
     count: number;
-    title:string;
-    increment:(value:number)=>void;
-    decrement:(value:number)=>void;
-    posts:Post[];
-    getPost:()=>string;
+    title: string;
+    increment: (value: number) => void;
+    decrement: (value: number) => void;
+    posts: Post[];
+    getPost: () => string;
 }
-export const useCounterStore = create<counterState>((set) => ({
+export const useCounterStore = create<counterState>()(immer((set) => ({
     count: 10,
-    title:'uno',
-    increment:(value:number)=>set(state=>({//actualizar el estado
-        count:state.count + value
+    title: 'uno',
+    increment: (value: number) => set(state => ({//actualizar el estado
+        count: state.count + value
     })),
-    decrement:(value:number)=>set(state=>({
-        count:state.count - value
+    decrement: (value: number) => set(state => ({
+        count: state.count - value
     })),
-    posts:[],
-    getPost:async()=>{ //peticiones asyncronas
-        const post=await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
-        set(state=>({
-            ...state,
-            posts:post
+    posts: [],
+    getPost: async () => { //peticiones asyncronas
+        const post = await (await fetch('https://jsonplaceholder.typicode.com/posts')).json();
+        set((state => {
+            state.posts = post
         }))
     }
-}))
+})))
 
